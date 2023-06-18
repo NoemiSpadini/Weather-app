c31bdaeeb65b96a50573ef915faf244cfed1f6da
NoemiSpadini <noemispadini94@gmail.com>
2023-06-18 12:07:01 +0200

    city default

--- src/index.js
+++ src/index.js
@@ -189,3 +189,5 @@ fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
+
+searchBox("Barcelona");
