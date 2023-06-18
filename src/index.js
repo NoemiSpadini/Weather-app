6b876d21603703120183951c826ec51eb66c54d6
NoemiSpadini <noemispadini94@gmail.com>
2023-06-18 11:59:44 +0200

    API key changed

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ function formatDate(date) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+  let apiKey = "be81f193e065bf5feb2d944c7336968b";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
